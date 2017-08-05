6862691f470488122bbde60b660adeae42d3494b
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-08-05 18:45:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML='Edited text'
 
 //edit image madi
 var img = document.getElementById('madi')
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 100;
+        img.style.marginLeft= marginLeft + 'px';
+
+}
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
 }
