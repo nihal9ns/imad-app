bac1b952669caf75a36ed7cfa2283d1eaf41e3dc
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-08-05 19:12:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,10 @@ console.log('Loaded!');
 
 //edit the content of div id="main-text"
 var element = document.getElementById('main-text')
-//element.innerHTML='Edited text'
+element.innerHTML='Edited text'
 
 //edit image madi
-/*var img = document.getElementById('madi')
+var img = document.getElementById('madi')
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 1;
@@ -14,4 +14,4 @@ function moveRight(){
 }
 img.onclick = function(){
     var interval = setInterval(moveRight,50);
-} */
+} 
