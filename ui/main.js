a2c8a413b5e1949eb860a6db6334a67c041b4250
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-08-05 20:03:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,3 +48,18 @@ request.onreadystatechange = function(){
     request.send(null);
 };
 
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit-btn');
+submit.onclick = function(){
+    //make a request to the server and send the name
+    //capture the list of names and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0 ; i < names.length ; i++){
+        list + = '<li>' + names[i] + '</li>';
+    }
+    var ul = getElementById('namelist');
+    ul.innerHTML = list;
+}
