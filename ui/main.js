377158a8aea3237a0f1e163758d29072626a0796
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-08-05 19:13:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //edit the content of div id="main-text"
 var element = document.getElementById('main-text')
-element.innerHTML='Edited text'
+//element.innerHTML='Edited text'
 
 //edit image madi
 var img = document.getElementById('madi')
