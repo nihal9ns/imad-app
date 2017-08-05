76109826f692c89a80a88cf5a9c1feee9037017c
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-08-05 18:37:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+
+//edit the content of div id="main-text"
 var element = document.getElementById('main-text')
 element.innerHTML='Edited text'
+
+//edit image madi
+var img = document.getElementById('madi')
+img.onClick = function(){
+    img.style.marginLeft='100px';
+}
