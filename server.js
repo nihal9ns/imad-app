16d158ee26457aba33db3edadc2b252148d005bc
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-10-15 15:27:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -168,16 +168,6 @@ function createTemplate (data){
 return htmlTemplate;
 }
 
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
-
 //--------------------------------------------
 //My APP
 //--------------------------------------------
@@ -222,6 +212,7 @@ app.get('/:placeName',function(req,res){
 	var placeName = req.params.placeName;
 	res.send(createTemplate(places[placeName]));
 });
+
 
 app.get('/style.css',function(req,res) {
 	res.sendFile(__dirname + '/style.css');
@@ -312,3 +303,14 @@ function createTemplate(data){
 
 	return htmlTemplate;
 }
+
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
