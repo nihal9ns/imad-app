6425c9b2cf0697a1b6c0e7fcbb8573ed6380bbba
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-10-15 16:51:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,7 @@ img.onclick = function() {
 /*var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
+    //make a request to the counter endpoint
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
