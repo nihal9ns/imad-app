7903a38f130780c5c16b7d45ae9d313d5b065815
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-10-16 10:25:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,8 +22,14 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     //How do we create a hash?
-    var hashed = crypto.pbkdf2Sync(input,salt,100000,512,'sha512');
-    return hashed.toString('hex');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')];
+    
+    
+    //md5
+    //"password" -> sfdsafnakrn345u894q3u8rhejf328432809ru
+    //"password-this-is-some-random-string" -> dnkfja89345jndsbfjwe893282349084sddfgafga
+    //"password" -> "password-this-is-a-salt" -> <hash> -> <hash> * 10k times.
 }
 
 app.get('/hash/:input',function(req,res){
