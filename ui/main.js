19b0b9e274c24434e6edc6da9cad33c92514291c
nihal9ns <30690618+nihal9ns@users.noreply.github.com>
2017-10-21 16:22:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -82,6 +82,45 @@ request.onreadystatechange = function(){
     request.open('GET','http://nihal9ns.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
+//-------------------------------------------------------------------------------------------------
+//Login
+//-------------------------------------------------------------------------------------------------
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+   var request = new XMLHttpRequest();
+
+//capture the response and store it in a variable
+request.onreadystatechange = function(){
+    if(request.readyState == XMLHttpRequest.DONE){
+        //do some action
+        if(request.status == 200){
+                console.log('User logged in!');
+                alert('Logged in successfully!');
+              }
+              else if(request.status === 403){
+                  alert('username/password is incorrect...');
+              }
+              else if(request.status === 500){
+                  alert('something went wrong on the server...');
+              }
+            }
+        }
+         //not done yet
+    };
+    //make the response
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://nihal9ns.imad.hasura-app.io/login',true);
+    request.setRequestHeader({'Content-Type','application/json'});
+    request.send(JSON.stringify({username : username,password : password}));
+};
+
+
+
+
 
 
 
